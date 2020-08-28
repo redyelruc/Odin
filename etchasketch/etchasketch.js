7504fb83ac0632dae49683e3c650b3eb7f8ef3cf
Aidan <redyelruc1@hotmail.com>
2020-08-28 17:17:28 +0100

    Sketchpad working, need to add grid size control, automated header, manual controls like the original game.

--- etchasketch/etchasketch.js
+++ etchasketch/etchasketch.js
@@ -1,22 +1,66 @@
 
+let paintable = false;
+let eraseMode = false;
+let defaultColor = "#009900";
+let fillColor;
 
-function addGridToSketchpad(size){
-    for (let i = 0; i < size; i ++){
-        makeRow(size);
-    }
+
+window.addEventListener("load", startup());
+
+function startup() {
+    let colorWell = document.querySelector("#color");
+    colorWell.value = defaultColor;
+    fillColor = defaultColor;
+    colorWell.addEventListener("change", () => {
+        fillColor = event.target.value}, false);
+    colorWell.select();
+    makeGrid(16);
+  }
+
+
+function setColor(){
+    let input = document.getElementById("color");
+    fillColor = input.getAttribute('value');
 }
 
-function makeRow(size){
+
+  function makeGrid(size) {
     let sketchpad = document.getElementById("sketchpad");
-    console.log(sketchpad);
-    for (let i=0;i<size;i++){
-        let cell = document.createElement('div');
-        cell.id = "cell";
-        cell.style.border = "1px solid black";
-        let width = size + "fr";
-        cell.width = width;
-        sketchpad.appendChild(cell); 
+    sketchpad.style.setProperty('--grid-rows', size);
+    sketchpad.style.setProperty('--grid-cols', size);
+    for (c = 0; c < (size * size); c++) {
+        let cell = document.createElement("div");
+        cell.style.border = "1px solid silver";
+        cell.addEventListener("click", () => {paintCell(cell)});
+        cell.addEventListener("mousedown", () => {paintable = true});  
+        cell.addEventListener("mouseup", () => {paintable = false});
+        cell.addEventListener("mouseover", () => {if (paintable) {paintCell(cell)}});
+        sketchpad.appendChild(cell).className = "grid-item";
+    };
+}
+
+
+function paintCell(cell){
+    if (eraseMode){
+        cell.style['background-color'] = "white";
+    } else{
+        cell.style['background-color'] = fillColor;
+    }   
+}
+
+
+function toggleEraseMode(){
+    let button = document.getElementById("erase-toggle");
+    if (!eraseMode){
+        eraseMode = true;
+        button.style["font-weight"] = "bold";
+        button.style["background-color"] = "#ffcccc";
+        button.style.border = "1px solid red";
+    } else {
+        eraseMode = false;
+        button.style["font-weight"] = "normal";
+        button.style["background-color"] = "white";
+        button.style.border = "1px solid black";
     }
 }
 
-addGridToSketchpad(10);
