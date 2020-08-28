4293e3faba1b3fc060a8c34c2daf4260b794da98
Aidan <redyelruc1@hotmail.com>
2020-08-28 12:45:37 +0100

    Set up basic HTML

--- etchasketch/etchasketch.js
+++ etchasketch/etchasketch.js
@@ -1,6 +1,22 @@
+
+
 function addGridToSketchpad(size){
-    sketchpad = document.getElementById("sketchpad");
     for (let i = 0; i < size; i ++){
-        // add divs
+        makeRow(size);
     }
 }
+
+function makeRow(size){
+    let sketchpad = document.getElementById("sketchpad");
+    console.log(sketchpad);
+    for (let i=0;i<size;i++){
+        let cell = document.createElement('div');
+        cell.id = "cell";
+        cell.style.border = "1px solid black";
+        let width = size + "fr";
+        cell.width = width;
+        sketchpad.appendChild(cell); 
+    }
+}
+
+addGridToSketchpad(10);
